fe62b2a4ba9d348bc26e0387cccac1ccd8d29f39
Sandy <sandy.congreve@cmeducations.se>
2022-10-05 09:13:29 +0200

    Added contact form with styling

--- components/About.jsx
+++ components/About.jsx
@@ -32,7 +32,7 @@ function About() {
         <div className="w-full h-auto m-auto shadow-xl shadow-gray-400 rounded-xl flex items-center justify-center p-4 hover:scale-105 ease-in duration-300">
           <Image
             className="rounded-xl"
-            src="/../public/assets/about.jpg"
+            src="/../public/assets/profileme.jpg"
             alt="/"
             width="500%"
             height="500%"
